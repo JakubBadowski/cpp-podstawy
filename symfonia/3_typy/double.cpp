a1a295df30545d4028da192b75c3d6ccf0362657
Jakub Badowski <badowski@inig.local>
2016-12-30 19:07:04 +0100

    symfonia c++ przyklady

---
+++ symfonia/3_typy/double.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "Stale doslowne - double" << endl;
+
+	double zmm;
+
+	// zmm = 1.4F;
+	// zmm = 10.4e5;
+	// zmm = 8e6;
+	// zmm = -12.;
+	zmm = .12;
+
+	cout << "Zmienna to = " << zmm << endl;
+
+	return 0;
+}
---
+++ symfonia/3_typy/int.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Stale doslowne - int" << endl;
+	int zmm;
+
+	// zmm = 10;
+	// zmm = 010;
+	zmm = 0x10;
+
+	cout << "Zmianna = " << zmm << endl;
+	return 0;
+}
